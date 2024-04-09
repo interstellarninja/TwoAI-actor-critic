421609800a2d137e04878de0c9074fffe44777f9
interstellarninja <ninjainterstellar@gmail.com>
2024-04-09 03:56:58 -0400

    adding actor, critic pydantic objects

--- src/twoai/utils.py
+++ src/twoai/utils.py
@@ -1,4 +1,5 @@
 from typing import TypedDict, Annotated
+from pydantic import BaseModel, Field
 
 class Agent(TypedDict):
     name: str # the name of the agent
@@ -6,3 +7,11 @@ class Agent(TypedDict):
     model: str # optional, model to use for this specific agent, if not provided use default
 
 AgentDetails = tuple[Agent, Agent]
+
+class Actor(BaseModel):
+    thought: str = Field(..., description="scratchpad for step-by-step reasoning")
+    action: str = Field(..., description="actions to be exectued to accomplish the task")
+
+class Critic(BaseModel):
+    evaluation: str = Field(..., description="evaluation of the actor's actions")
+    feedback: str = Field(..., description="constructive feedback to revise actions")
